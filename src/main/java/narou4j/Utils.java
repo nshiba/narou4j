1acb82716bb944e08d887015ee54d5c926c4dea1
Naoya Shibahara <naoya@cps.im.dendai.ac.jp>
2016-04-11 12:23:32 +0900

    Add ungzip method

--- src/main/java/narou4j/Utils.java
+++ src/main/java/narou4j/Utils.java
@@ -4,20 +4,49 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import okhttp3.Response;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
+import java.util.zip.GZIPInputStream;
 
 public class Utils {
 
-    public static List<Novel> response2Json(Response response) {
+    public static List<Novel> response2Json(Response response, boolean isGzip) {
         ObjectMapper mapper = new ObjectMapper();
+        String str = null;
+
+        if (isGzip) {
+            try {
+                try (BufferedReader br = new BufferedReader(
+                        new InputStreamReader(new GZIPInputStream(response.body().byteStream())))) {
+
+                    StringBuilder builder = new StringBuilder();
+                    String line;
+                    while ((line = br.readLine()) != null) {
+                        builder.append(line);
+                    }
+
+                    str = builder.toString();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            try {
+                str = response.body().string();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        List<Novel> novels = null;
         try {
-            String str = response.body().string();
-            List<Novel> novels = mapper.readValue(str, new TypeReference<List<Novel>>(){});
-            return novels;
+            novels = mapper.readValue(str, new TypeReference<List<Novel>>() {
+            });
         } catch (IOException e) {
             e.printStackTrace();
-            return null;
         }
+
+        return novels;
     }
 }
